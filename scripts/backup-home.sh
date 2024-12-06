b34feff977f9823b207503902a1b27ed18294069
buhfur <ryanmcvicker@protonmail.com>
2024-12-05 23:16:25 -0500

    Auto Commit: Thu Dec  5 11:16:25 PM EST 2024

--- scripts/backup-home.sh
+++ scripts/backup-home.sh
@@ -2,23 +2,25 @@
 
 # Script that uses rsync to backup users home directory 
 
-BACKUP_DIR="/mnt/backups/backups/home-backups"
+ROOT_BACKUP_DIR="/mnt/backups/backups/home-backups"
 
 
-if [ ! -d $BACKUP_DIR/"$(hostname)-backups" ]; then # Creates users home backup dir if not present
+if [ ! -d $ROOT_BACKUP_DIR/"$(hostname)-backups" ]; then # Creates users home backup dir if not present
     echo "INFO: Creating backup directory "
-    mkdir $BACKUP_DIR/"$(hostname)-backups"
+    mkdir $ROOT_BACKUP_DIR/"$(hostname)-backups"
     if [ $? -eq 0 ]; then
         echo "INFO: Target backup directory created successfully "
     fi
 fi
 
 
-BACKUP_DIR="$BACKUP_DIR/$(hostname)-backups"
+BACKUP_DIR="$ROOT_BACKUP_DIR/$(hostname)-backups" # name of backup dir withing ROOT_BACKUP_DIR
 if [ -f /var/log/backup.log ]; then # create log file if not present 
     touch /var/log/backup.log
 fi
-rsync -avzh --progress /home/* $BACKUP_DIR > /var/log/backup.log
+# rsync -avzh --progress /home/* $BACKUP_DIR > /var/log/backup.log
+tar cvzf $BACKUP_DIR/backup-$(date +"%Y-%m-%d_%H-%M-%S").tar.gz /home/
+
 if [ $? -eq 0 ]; then
     echo "INFO: Home diretory backed up successfully " 
 else
