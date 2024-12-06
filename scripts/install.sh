74b282d14b700704b180795808084c518d5b2a99
buhfur <ryanmcvicker@protonmail.com>
2024-12-05 22:22:52 -0500

    test commit for remote backup scripts and units

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,17 +2,22 @@
 
 # Copy and activate systemd units 
 
-if [ ! -f /etc/systemd/system/backup.service ]; then
+if [[ ! -f /etc/systemd/system/backup.service || ! -f /etc/systemd/system/remote-backup.service ]]; then
+    cp ../units/backup.service /etc/systemd/system/
     cp ../units/backup.service /etc/systemd/system/
 fi 
 
-if [ ! -f /etc/systemd/system/backup.timer ]; then
+if [[ ! -f /etc/systemd/system/backup.timer || ! -f /etc/systemd/system/remote-backup.timer ]]; then
     cp ../units/backup.timer /etc/systemd/system/
+    cp ../units/remote-backup.timer /etc/systemd/system/
 fi 
+
 
 if [[ -f /etc/systemd/system/backup.timer || -f /etc/systemd/system/backup.service ]]; then # Activate and enable units , then daemon-reload 
     sudo systemctl enable backup.service
     sudo systemctl enable backup.timer
+    sudo systemctl enable remote-backup.service
+    sudo systemctl enable remote-backup.timer
     if [ $? -eq 0 ]; then
         echo "INFO: Systemd unit and timer have been copied and enabled successfully" 
     else
@@ -22,11 +27,14 @@ if [[ -f /etc/systemd/system/backup.timer || -f /etc/systemd/system/backup.servi
 
 fi
 
-# Copy backup script to /usr/local/sbin 
+# Copy local and remote backup script to /usr/local/sbin 
 
 sudo cp backup-home.sh /usr/local/sbin/
+sudo cp remote-backup-home.sh /usr/local/sbin/
+
 if [ $? -eq 0 ]; then
     echo "INFO: backup-home.sh script was successfully copied to /usr/local/sbin"
+    echo "INFO: remote-backup-home.sh script was successfully copied to /usr/local/sbin"
 else
     echo "ERROR: Units were not able to be activated , please see logging for further info "
 fi
---
+++ scripts/remote-backup-home.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# template script which moves specifed backups in directory to corresponding location on remote backups 
+
+
+LBACKUP_DIR="" # change this to the local directory you would like backed up 
+RBACKUP_DIR="" # same but with the name of the remote backup location  
+
+if [ -d $BACKUP_DIR ]; then
+    # Copy directory using rclone and write to log file 
+    rclone copy $BACKUP_DIR
+    if [ $? -eq 0 ]; then
+        echo "INFO: All files in target $LBACKUP_DIR have been successfully copied to $RBACKUP_DIR"
+fi 
+
+
+
